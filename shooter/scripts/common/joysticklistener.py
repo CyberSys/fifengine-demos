e21a2fc269667985be310eb5d2ea418bffcf1c8e
LinuxDonald <linuxdonald@posteo.de>
2017-11-25 19:52:03 +0100

    fix mix of space and tabs

--- shooter/scripts/common/joysticklistener.py
+++ shooter/scripts/common/joysticklistener.py
@@ -31,16 +31,16 @@ class JoystickListener(fife.IJoystickListener):
 		self.eventmanager.addJoystickListener(self)
 
 	def axisMotion(self, evt):
-                axisX = 0
-                axisY = 1
-                if evt.isController:
-                        axisX = fife.Joystick.CONTOLLER_AXIS_LEFTX
-                        axisY = fife.Joystick.CONTOLLER_AXIS_LEFTY
+		axisX = 0
+		axisY = 1
+		if evt.isController:
+			axisX = fife.Joystick.CONTOLLER_AXIS_LEFTX
+			axisY = fife.Joystick.CONTOLLER_AXIS_LEFTY
 		if evt.getAxis() == axisX:
 			if evt.getAxisValue() < 0:
 				self.world._keystate['LEFT'] = True
 			elif evt.getAxisValue() > 0:
-				self.world._keystate['RIGHT'] = True
+			 	self.world._keystate['RIGHT'] = True
 			else:
 				self.world._keystate['LEFT'] = False
 				self.world._keystate['RIGHT'] = False
@@ -57,13 +57,13 @@ class JoystickListener(fife.IJoystickListener):
 		pass
 
 	def buttonPressed(self, evt):
-                button = 0
+		button = 0
 		if evt.isController(): button = fife.Joystick.CONTOLLER_BUTTON_A
 		if evt.getButton() == button:
 			self.world._keystate['SPACE'] = True
 
 	def buttonReleased(self, evt):
-                button = 0
+		button = 0
 		if evt.isController(): button = fife.Joystick.CONTOLLER_BUTTON_A
 		if evt.getButton() == button:
 			self.world._keystate['SPACE'] = False
